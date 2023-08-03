a3e0f99d8c199b817497457a6de65efd5e0d0f1b
Alex Oliveira <alexoliveira7x@gmail.com>
2023-08-03 17:16:51 +0100

    chore: update

--- packages/core/src/services/get-posts/index.ts
+++ packages/core/src/services/get-posts/index.ts
@@ -9,7 +9,7 @@ export default async function getPosts(): Promise<Post[]> {
   const query = `
     query($owner: String!, $repo: String!, $cursor: String) {
       repository(owner: $owner, name: $repo) {
-        issues(first: 100, states: CLOSED, after: $cursor) {
+        issues(first: 100, states: CLOSED, labels: ["NextJS"], after: $cursor) {
           pageInfo {
             endCursor
             hasNextPage
