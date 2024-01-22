95638e7a83d9370ff51a7221f2069577c41487a0
Alex Oliveira <alexoliveira7x@gmail.com>
2024-01-22 23:12:52 +0000

    add clarity

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
+import { ClarityScript } from '../components/Clarity'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <ClarityScript />
+    </>
+  )
 }
